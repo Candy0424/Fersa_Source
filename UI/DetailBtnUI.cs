9ee595db09ed49fa3c4cb5ba99abdcd210feb2e8
candy <157550346+Candy0424@users.noreply.github.com>
2026-05-06 19:35:17 +0900

    Code

---
+++ UI/DetailBtnUI.cs
@@ -0,0 +1,56 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using YIS.Code.Defines;
+using YIS.Code.Items;
+
+namespace YIS.Code.Combat
+{
+    public class DetailBtnUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [field: SerializeField] public DetailUIType DetailType { get; private set; }
+        [field: SerializeField] public DetailDataSO DetailData { get; private set; }
+        [field: SerializeField] public Button Btn { get; private set; }
+        [SerializeField] private TextMeshProUGUI uiName;
+        [SerializeField] private TextMeshProUGUI priceText;
+        [SerializeField] private Image icon;
+        [SerializeField] private Image goldIcon;
+        [SerializeField] private GameObject selectBox;
+
+        private uint _price;
+
+        public uint Price => _price;
+
+        public void InitData(DetailDataSO detailData)
+        {
+            DetailData = detailData;
+            SetDetailData();
+            selectBox?.SetActive(false);
+            _price = detailData.basePrice;
+        }
+        
+        private void SetDetailData()
+        {
+            _price = DetailData.basePrice;
+            uiName.SetText(DetailData.detailName);
+            icon.sprite = DetailData.detailIcon;
+
+            if (DetailData.useGold)
+            {
+                //goldIcon.sprite = DetailData.cacheIcon;
+                //priceText.SetText(_price.ToString());
+            }
+        }
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            selectBox.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            selectBox.SetActive(false);
+        }
+    }
+}
